16a714792b95c0cfd4d5d12ad429cbdb4317f587
MurotxonovAnvarxon <anvarxonmurotxonov777@gmail.com>
2026-03-30 10:29:50 +0500

    fix: language view fixed item

--- features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/localization/LocalizationNode.kt
+++ features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/localization/LocalizationNode.kt
@@ -14,6 +14,7 @@ import android.app.Activity
 import androidx.activity.compose.LocalActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.plugin.Plugin
@@ -21,6 +22,7 @@ import dev.zacsweers.metro.Assisted
 import dev.zacsweers.metro.AssistedInject
 import io.element.android.annotations.ContributesNode
 import io.element.android.compound.theme.ElementTheme
+import io.element.android.features.preferences.impl.R
 import io.element.android.libraries.androidutils.browser.openUrlInChromeCustomTab
 import io.element.android.libraries.architecture.callback
 import io.element.android.libraries.di.SessionScope
@@ -30,28 +32,24 @@ import io.element.android.libraries.di.SessionScope
 class LocalizationNode(
     @Assisted buildContext: BuildContext,
     @Assisted plugins: List<Plugin>,
-    private val presenter: AboutPresenter,
 ) : Node(buildContext, plugins = plugins) {
     interface Callback : Plugin {
     }
 
     private val callback: Callback = callback()
 
-    private fun onElementLegalClick(
-        activity: Activity,
-        darkTheme: Boolean,
-        elementLegal: ElementLegal,
-    ) {
-        activity.openUrlInChromeCustomTab(null, darkTheme, elementLegal.url)
-    }
+
 
     @Composable
     override fun View(modifier: Modifier) {
         val activity = requireNotNull(LocalActivity.current)
         val isDark = ElementTheme.isLightTheme.not()
-        val state = presenter.present()
+
+
         LocalizationView(
-//            state = state,
+            title = stringResource(R.string.language),
+            onBackClick = ::navigateUp,
+            //            state = state,
 //            onBackClick = ::navigateUp,
 //            onElementLegalClick = { elementLegal ->
 //                onElementLegalClick(activity, isDark, elementLegal)
--- features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/localization/LocalizationView.kt
+++ features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/localization/LocalizationView.kt
@@ -1,40 +1,92 @@
-/*
- * Copyright (c) 2026 Element Creations Ltd.
- *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
- * Please see LICENSE files in the repository root for full details.
- */
-
 package io.element.android.features.preferences.impl.localization
 
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import io.element.android.compound.theme.ElementTheme
+import io.element.android.libraries.designsystem.components.button.BackButton
 import io.element.android.libraries.designsystem.preview.PreviewsDayNight
+import io.element.android.libraries.designsystem.theme.aliasScreenTitle
 import io.element.android.libraries.designsystem.theme.components.Scaffold
+import io.element.android.libraries.designsystem.theme.components.Text
+import io.element.android.libraries.designsystem.theme.components.TopAppBar
 
 @Composable
-fun LocalizationView() {
-
+fun LocalizationView(
+    title: String,
+    onBackClick: () -> Unit,
+) {
+    val languages = listOf("Uzbek", "English", "Russian")
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
             .systemBarsPadding()
             .imePadding(),
         content = {
+            Box(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(
+                    state = rememberLazyListState(),
+                ) {
 
-        }
+                    items(count = languages.size, itemContent = { item ->
+                        LocalizationItem(name = item.toString())
+                    })
+                }
+            }
+        },
+        topBar = {
+            LocalizationTopAppBar(
+                title = title,
+                onBackClick = onBackClick,
+            )
+        },
     )
+}
+
+@Composable
+fun LocalizationItem(name: String) {
+    Box() {
+        Text("language")
+    }
 }
 
 @Composable
 @PreviewsDayNight
 @Preview
 fun LocalizationViewPreview() {
-    LocalizationView()
+    LocalizationView(title = "Language", {})
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun LocalizationTopAppBar(
+    title: String,
+    onBackClick: () -> Unit,
+) {
+    TopAppBar(
+        navigationIcon = {
+            BackButton(onClick = onBackClick)
+        },
+        title = {
+            Text(
+                modifier = Modifier.semantics {
+                    heading()
+                },
+                text = title,
+                style = ElementTheme.typography.aliasScreenTitle,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    )
 }
